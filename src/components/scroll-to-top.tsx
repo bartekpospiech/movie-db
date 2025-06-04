6b0592cf04c50cf5215d7c3b01da3ca21637cce6
Bartosz Pośpiech <bartek.codes@gmail.com>
2025-06-04 22:07:14 +0200

    chore: update scroll to top

--- src/components/scroll-to-top.tsx
+++ src/components/scroll-to-top.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router'
 
-export const ScrollToTop = ({ page }: { page: number }) => {
+export const ScrollToTop = ({ page }: { page?: number }) => {
   const { pathname } = useLocation()
 
   useEffect(() => {
