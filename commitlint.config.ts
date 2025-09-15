60de7046653d5a6a6de519c3ba4b5eaf9f6e1f72
Bartosz Pośpiech <bartek.codes@gmail.com>
2025-09-15 22:39:21 +0200

    chore: update commitlint config

---
+++ commitlint.config.ts
@@ -0,0 +1,14 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+  },
+}
