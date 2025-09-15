803b3e57b5240cf2c40800970df8cac8e5b1f856
Bartosz Pośpiech <bartek.codes@gmail.com>
2025-09-15 22:43:37 +0200

    chore: update commitlint config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,14 +1,16 @@
-export default {
+import type { UserConfig } from '@commitlint/types'
+
+import { RuleConfigSeverity } from '@commitlint/types'
+
+const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 100],
-    'footer-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'always', 100],
-    'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'type-enum': [
-      2,
-      'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
-    ],
+    'type-enum': [RuleConfigSeverity.Error, 'always', ['feat', 'fix', 'docs', 'refactor', 'chore', 'merge']],
+    'type-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
+    'type-empty': [RuleConfigSeverity.Error, 'never'],
+    'subject-max-length': [RuleConfigSeverity.Error, 'always', 120],
+    'subject-min-length': [RuleConfigSeverity.Error, 'always', 3],
   },
 }
+
+module.exports = Configuration
